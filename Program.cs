44b6dea38e6fdebf6c144190a89299181737c74b
d4niel <olayajuandaniel8@gmail.com>
2023-10-01 13:43:54 -0500

    base dir

--- Program.cs
+++ Program.cs
@@ -32,7 +32,9 @@ var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
-
+app.MapGet("/base", ()=>{
+    return AppDomain.CurrentDomain.BaseDirectory;
+});
 app.MapGet("/getfiles", () =>
 {
     List<string> fileList = new List<string>();
